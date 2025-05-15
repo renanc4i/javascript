d18d769303b3428e7e5897cb3854d6a58148dce0
renanc4i <182712327+renanc4i@users.noreply.github.com>
2025-05-15 10:31:15 -0300

    Primeiro código feito sozinho

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 30
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassou o limete de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
